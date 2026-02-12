6520eb34096f2503a983e31856e6d33ae29440b7
thabitalgodai <sabet122x@gmail.com>
2026-02-12 19:16:48 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyD7A8-DKqjzeKEi9BavHLDJ18J_PT6bsT0",
+    authDomain: "mozkrh2.firebaseapp.com",
+    databaseURL: "https://mozkrh2-default-rtdb.firebaseio.com",
+    projectId: "mozkrh2",
+    storageBucket: "mozkrh2.appspot.com",
+    messagingSenderId: "1072010410647",
+    appId: "1:1072010410647:web:603ddf7d867db870b787d2"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
